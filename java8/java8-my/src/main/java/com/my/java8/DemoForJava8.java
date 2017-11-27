9b472193e834d666db271dfb26f63a9b2b54a044
cuimiao <373013128@qq.com>
2017-11-28 00:23:44 +0800

    study java8 one day

--- java8/java8-my/src/main/java/com/my/java8/DemoForJava8.java
+++ java8/java8-my/src/main/java/com/my/java8/DemoForJava8.java
@@ -1,9 +1,134 @@
 package com.my.java8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 /**
  * @auther cuimiao
  * @date 2017/11/27/027  21:15
- * @deprecated
+ * @deprecated java8 Lambda表达式+流 demo.
  */
 public class DemoForJava8 {
+    public static void main(String[] args) {
+        //example - 1
+        // Java 8之前：
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Before Java8, too much code for too little to do");
+            }
+        }).start();
+        //Java 8方式：
+        new Thread(() -> System.out.println("In Java8, Lambda expression rocks !!")).start();
+
+        //example - 2
+        // Java 8之前：
+        List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        for (String feature : features) {
+            System.out.println(feature);
+        }
+        // Java 8之后：
+        //List features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        features.forEach(n -> System.out.println(n));
+
+        // 使用Java 8的方法引用更方便，方法引用由::双冒号操作符标示，
+        // 看起来像C++的作用域解析运算符
+        features.forEach(System.out::println);
+
+        //example - 3
+        List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
+
+        System.out.println("Languages which starts with J :");
+        filter(languages, (str)->str.startsWith("J"));
+
+        System.out.println("Languages which ends with a ");
+        filter(languages, (str)->str.endsWith("a"));
+
+        System.out.println("Print all languages :");
+        filter(languages, (str)->true);
+
+        System.out.println("Print no language : ");
+        filter(languages, (str)->false);
+
+        System.out.println("Print language whose length greater than 4:");
+        filter(languages, (str)->str.length() > 4);
+
+        //example - 3
+        // 甚至可以用and()、or()和xor()逻辑函数来合并Predicate，
+        // 例如要找到所有以J开始，长度为四个字母的名字，你可以合并两个Predicate并传入
+        Predicate<String> startsWithJ = (n) -> n.startsWith("J");
+        Predicate<String> fourLetterLong = (n) -> n.length() == 4;
+        languages.stream()
+                .filter(startsWithJ.and(fourLetterLong))
+                .forEach((n) -> System.out.print("nName, which starts with 'J' and four letter long is : " + n));
+
+        //example - 4
+        // 不使用lambda表达式为每个订单加上12%的税
+        List<Integer> costBeforeTax = Arrays.asList(100, 200, 300, 400, 500);
+        for (Integer cost : costBeforeTax) {
+            double price = cost + .12*cost;
+            System.out.println(price);
+        }
+
+        // 使用lambda表达式
+        costBeforeTax.stream().map((cost) -> cost + .12*cost).forEach(System.out::println);
+
+        //example - 5
+        // 为每个订单加上12%的税
+        // 老方法：
+        double total = 0;
+        for (Integer cost : costBeforeTax) {
+            double price = cost + .12*cost;
+            total = total + price;
+        }
+        System.out.println("Total : " + total);
+
+        // 新方法：
+        double bill = costBeforeTax.stream().map((cost) -> cost + .12*cost).reduce((sum, cost) -> sum + cost).get();
+        System.out.println("Total : " + bill);
+
+        //example - 6
+        // 创建一个字符串列表，每个字符串长度大于2
+        List<String> strList = new ArrayList<>();
+        List<String> filtered = strList.stream().filter(x -> x.length()> 2).collect(Collectors.toList());
+        System.out.printf("Original List : %s, filtered list : %s %n", strList, filtered);
+
+        //example - 7
+        // 将字符串换成大写并用逗号链接起来
+        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
+        String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", "));
+        System.out.println(G7Countries);
+
+        //example - 8
+        // 本例展示了如何利用流的 distinct() 方法来对集合进行去重。
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream().map( i -> i*i).distinct().collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+
+        //example - 9
+        //获取数字的个数、最小值、最大值、总和以及平均值
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats.getMax());
+        System.out.println("Lowest prime number in List : " + stats.getMin());
+        System.out.println("Sum of all prime numbers : " + stats.getSum());
+        System.out.println("Average of all prime numbers : " + stats.getAverage());
+    }
+
+    /**
+     * 根据lambda表达式过滤names.
+     * @param names 名字列表.
+     * @param condition 筛选条件的Lambda表达式.
+     */
+    public static void filter(List<String> names, Predicate<String> condition) {
+        for(String name: names)  {
+            if(condition.test(name)) {
+                System.out.println(name + " ");
+            }
+        }
+    }
 }
